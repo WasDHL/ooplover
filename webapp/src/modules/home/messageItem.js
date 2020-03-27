61176aac07526e1beeeda847a758cfa464ff23a5
wanghuan <huan.wang@chiefclouds.com>
2020-03-27 17:43:48 +0800

    Add Hide Model

--- webapp/src/modules/home/messageItem.js
+++ webapp/src/modules/home/messageItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import moment from 'moment';
 
+const adimn1Avator = require('./../../assets/avators/admin1.jpg');
+const adimn2Avator = require('./../../assets/avators/admin2.jpg');
+
 export default function MessageItem (props) {
     var message = props.message;
     var user = props.user;
     return (
         <div className="message-item">
+            <div className="message-avator">
+                <img src={user && user.id == 1 ? adimn1Avator : adimn2Avator} />
+            </div>
             <div className="message-header">
                 <span className="send-user">{user.name}</span>  <span className="send-date">{moment(message.createTime * 1000).format('YYYY-MM-DD HH:mm:ss')}</span></div>
             <div className="message-content">{message.content}</div>
--- webapp/src/modules/index.js
+++ webapp/src/modules/index.js
@@ -7,7 +7,42 @@ import Login from './login';
 import About from './about';
 //  <Router history={hashHistory} routes={routes} />
 
+window.GetDreamStatus = function () {
+    return window.localStorage.getItem('DREAM_STATUS');
+}
+
+window.expandStepCount = 0;
+window.refreshFlag = null;
+
+window.ExpandDream = function () {
+    window.expandStepCount = window.expandStepCount + 1;
+
+    if (window.expandStepCount > 2) {
+        window.localStorage.setItem('DREAM_STATUS', 'expanded');
+
+        document.getElementById('J_appHideModel').style.display = 'none';
+        document.getElementById('J_appCollapseBtn').style.display = 'block';
+        window.expandStepCount = 0;
+    }
+
+    window.clearTimeout(window.refreshFlag)
+    window.refreshFlag = setTimeout(function () {
+        window.expandStepCount = 0;
+    }, 1000);
+};
+
+window.CollapseDream = function () {
+    window.localStorage.setItem('DREAM_STATUS', 'collapsed');
+
+    document.getElementById('J_appHideModel').style.display = 'block';
+    document.getElementById('J_appCollapseBtn').style.display = 'none';
+};
+
 export default function RootModule (props) {
+    var expand = function () { window.ExpandDream(); }
+    var collapse = function () { window.CollapseDream(); }
+    var status = window.GetDreamStatus();
+
     return (
         <div id="J_appRouterContainer">
             <HashRouter>
@@ -17,6 +52,14 @@ export default function RootModule (props) {
                     <Route path="/about" component={About}></Route>
                 </Switch>
             </HashRouter>
+
+            <span id="J_appCollapseBtn" className="collapse-btn" style={{ 'display': status == 'expanded' ? 'block' : 'none' }} onClick={collapse}>Collapse BTN</span>
+            <div id="J_appHideModel" className="hide-model" style={{ 'display': status !== 'expanded' ? 'block' : 'none' }}>
+                <div>测试页面</div>
+                <div>面向对象 爱好者</div>
+                <div>作者: Harry</div>
+                <div><span onClick={expand}>面向对象(Object Oriented,OO)</span>是 <b>软件开发方法</b>。面向对象的概念和应用已超越了程序设计和软件开发，扩展到如数据库系统、交互式界面、应用结构、应用平台、分布式系统、网络管理结构、CAD技术、人工智能等领域。面向对象是一种对<b>现实世界理解和抽象的方法</b>，是计算机编程技术 [1]  发展到一定阶段后的产物。<span>（ -- 来自百度百科 ）</span></div>
+            </div>
         </div>
     );
 }
